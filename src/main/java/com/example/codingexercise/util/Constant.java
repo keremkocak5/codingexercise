5f09fef8dab3fa341410ec2fb6e4961108a2bd35
keremkocak5 <72099354+keremkocak5@users.noreply.github.com>
2025-04-07 09:42:37 +0100

    Fix constant and remove unused test

--- src/main/java/com/example/codingexercise/util/Constant.java
+++ src/main/java/com/example/codingexercise/util/Constant.java
@@ -1,8 +1,10 @@
 package com.example.codingexercise.util;
 
+import com.example.codingexercise.enums.CurrencyCode;
+
 public class Constant {
 
-    public static final String USD = "USD";
+    public static final String USD = CurrencyCode.USD.name();
 
     private Constant() {
     }
